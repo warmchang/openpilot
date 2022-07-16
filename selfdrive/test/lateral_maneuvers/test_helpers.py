35b719d5fc783ec303feeacb6acdc5752bd1d84f
nuwandavek <vivekaithal44@gmail.com>
2022-07-15 17:02:34 -0700

    finish up test v1

--- selfdrive/test/lateral_maneuvers/test_helpers.py
+++ selfdrive/test/lateral_maneuvers/test_helpers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cereal.messaging as messaging
 from selfdrive.modeld.constants import T_IDXS
+from selfdrive.controls.lib.drive_helpers import LAT_MPC_N as N
 from selfdrive.controls.lib.lateral_planner import LateralPlanner
 
 
@@ -9,10 +10,10 @@ class Plan:
     self.speed = speed
     self.yaw_rate = np.radians(yaw_rate)
     self.y_offset = y_offset
-    self.current_yaw_rate = current_yaw_rate
+    self.current_yaw_rate = np.radians(current_yaw_rate)
     self.dt = np.diff(T_IDXS, prepend=0.0)
 
-  def get_plan(self):
+  def get_plan_pts(self):
     plan_trajectory = {}
     yaw = np.cumsum(self.dt * self.yaw_rate)
     plan_trajectory['position'] = np.zeros((len(T_IDXS), 3))
@@ -35,23 +36,27 @@ class Plan:
     submsg.zStd = stds[:, 2].tolist()
 
   def get_messages(self):
-    plan = self.get_plan()
+    plan = self.get_plan_pts()
     sm = {}
     for message in ['carState', 'controlsState', 'modelV2']:
       sm[message] = messaging.new_message(message).__getattr__(message)
 
     sm['carState'].vEgo = self.speed
     sm['controlsState'].active = True
-    sm['controlsState'].curvature = self.current_yaw_rate / max(1.0, self.speed)
+    sm['controlsState'].curvature = float(self.current_yaw_rate / max(1.0, self.speed))
 
     for field in ['position', 'velocity', 'orientation', 'orientationRate']:
       self.put_vals(sm['modelV2'], plan, field)
 
     return sm
 
+  def run_planner(self):
+    plan_points = self.get_plan_pts()
+    sm = self.get_messages()
+    lp = LateralPlanner(use_lanelines=False)
+    lp.x0[3] = sm['controlsState'].curvature
+    lp.update(sm)
 
-def run_planner(sm):
-  lateral_planner = LateralPlanner(use_lanelines=False)
-  lateral_planner.x0[3] = sm['controlsState'].curvature
-  lateral_planner.update(sm)
-  return True
+    y_err = lp.lat_mpc.x_sol[:, 1] - plan_points['position'][:N + 1, 1]
+    lateral_acceleration = lp.lat_mpc.x_sol[:, 3] * self.speed**2
+    return y_err, lateral_acceleration
--- selfdrive/test/lateral_maneuvers/test_lateral.py
+++ selfdrive/test/lateral_maneuvers/test_lateral.py
@@ -1,18 +1,61 @@
 #!/usr/bin/env python3
 import unittest
-from selfdrive.test.lateral_maneuvers.test_helpers import Plan, run_planner
+import numpy as np
+from selfdrive.test.lateral_maneuvers.test_helpers import Plan
+
+Y_ERR_THRESH = 1  # m
+Y_ERR_THRESH_END = 0.5  # m
+LA_ERR_THRESH = 3.0  # m/s^2
 
 
 class LateralPlannerSingleStepTests(unittest.TestCase):
   """
-  Currently we only have single step tests.
-  Ideally we should have full trajectories, but that requires a simple simulator (dynamics) and
-  re-calculating the plan wrt ground-truth at each step like in xx.
+  Note:
+  1. Currently we only have single step tests. Ideally we should have full trajectories,
+  but that requires a simple simulator (dynamics) and re-calculating the plan wrt
+  ground-truth at each step like in xx.
+  2. The generated plan right now is very simple, and does not account for the variation
+  in rotation radius in cars.
+  Todo: Add some actual trajectories
+
   """
-  def straight_road(self):
-    sm = Plan(speed=30, yaw_rate=3, y_offset=0, current_yaw_rate=0).get_messages()
-    planner_result = run_planner(sm)
-    self.assertTrue(planner_result)
+  def check_all_asserts(self, args):
+    y_err, lat_acc = args
+    self.assertTrue(np.max(np.abs(y_err)) < Y_ERR_THRESH)
+    self.assertTrue(np.abs(y_err[-1]) < Y_ERR_THRESH_END)
+    self.assertTrue(np.max(np.abs(lat_acc)) < LA_ERR_THRESH)
+
+  def test_straight_road(self):
+    plan = Plan(speed=30, yaw_rate=0, y_offset=0, current_yaw_rate=0)
+    self.check_all_asserts(plan.run_planner())
+
+  def test_straight_road_offset(self):
+    plan = Plan(speed=30, yaw_rate=0, y_offset=0.5, current_yaw_rate=0)
+    self.check_all_asserts(plan.run_planner())
+
+  def test_high_speed_turn(self):
+    plan = Plan(speed=30, yaw_rate=3, y_offset=0, current_yaw_rate=0)
+    self.check_all_asserts(plan.run_planner())
+
+  def test_low_speed_turn(self):
+    plan = Plan(speed=5, yaw_rate=3, y_offset=0, current_yaw_rate=0)
+    self.check_all_asserts(plan.run_planner())
+
+  def test_offset_turn(self):
+    plan = Plan(speed=5, yaw_rate=3, y_offset=-0.5, current_yaw_rate=0)
+    self.check_all_asserts(plan.run_planner())
+
+  def test_sharp_turn(self):
+    plan = Plan(speed=10, yaw_rate=-10, y_offset=0, current_yaw_rate=0)
+    self.check_all_asserts(plan.run_planner())
+
+  def test_sharp_turn_already_turning(self):
+    plan = Plan(speed=10, yaw_rate=-10, y_offset=0, current_yaw_rate=-5)
+    self.check_all_asserts(plan.run_planner())
+
+  def test_sharp_turn_already_turning_offset(self):
+    plan = Plan(speed=10, yaw_rate=-10, y_offset=0.5, current_yaw_rate=-5)
+    self.check_all_asserts(plan.run_planner())
 
 
 if __name__ == "__main__":

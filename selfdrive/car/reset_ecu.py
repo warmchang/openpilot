50cfe9c32cdd81e6ed02bd2ac5e856ef308fb661
sshane <shane@smiskol.com>
2022-10-14 02:49:53 -0700

    script to reset ecu (for debugging on bolt)

---
+++ selfdrive/car/reset_ecu.py
@@ -0,0 +1,26 @@
+from selfdrive.car.isotp_parallel_query import IsoTpParallelQuery
+from system.swaglog import cloudlog
+
+
+def reset_ecu(logcan, sendcan, bus=0, addr=0x7d0, timeout=0.1, debug=False):
+  # ON GMLAN: This will only work if vehicle speed is 0 and engine is off
+  cloudlog.warning(f"ecu reset {hex(addr)} ...")
+
+  # GMLAN CPID $FD - ECU reset
+  request = b"\xfd\x40"
+
+  query = IsoTpParallelQuery(sendcan, logcan, bus, [addr], [request], [b""], debug=debug)
+  print(query.get_data(timeout))
+
+
+if __name__ == "__main__":
+  import time
+  import cereal.messaging as messaging
+  sendcan = messaging.pub_sock('sendcan')
+  logcan = messaging.sub_sock('can')
+  time.sleep(0.2)
+
+  # GM EPS
+  addr = 0x642 - 0x400
+
+  reset_ecu(logcan, sendcan, bus=0, addr=addr, timeout=0.5, debug=True)
